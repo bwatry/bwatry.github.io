88c61f68c152f62437b917cc73b2e2d8a2e07b7c
Brenna <>
2021-12-29 13:44:03 -0700

    Create Bio component

---
+++ src/components/Bio.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Bio() {
+    return <div>
+        <section className="bio-section white-section container-fluid">
+            <div className="row">
+                <div className="bio">
+                    <img className="about-me-img" src="Images/Art Deco Picture-white.png" alt="Picture of Brenna">
+                </div>
+                <div className="bio container-narrow col-lg-10">
+                    <h1>About Brenna</h1>
+                    <p>I've known since I was a child that I wanted to be a writer.
+                        I even got a degree in creative writing and published a few short
+                        stories. To pay the bills, I took a job writing science-based legal reports.
+                    </p>
+                    <p>And then, in an unusual and fortuitous turn of events, I got the
+                        chance to try my hand at creating, cleaning, and transforming a
+                        database. I took a few online classes so I'd know what I was doing.
+                        It was just VBA and SQL to begin with, but the spark was lit.
+                    </p>
+                    <p>(Not by VBA, ew.)</p>
+                    <p>SQL was nice enough, but I wanted to explore other languages, so I
+                        took a course in web development, and <em>that</em> was when I
+                        really fell in love. There were similarities not just between
+                        programming and writing, but also with other hobbies I loved:
+                        sewing, jigsaw puzzles, and art.
+                    </p>
+                    <p>I made this web site to demonstrate to myself and others what I
+                        could do. I hope I've caught your interest!
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+}
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ export default function Skills() {
             <div className="row">
                 <Skill title="Programmer" details="I have experience in HTML, CSS, JavaScript, SQL, NoSQL, & Python, as well as associated libraries." />
                 <Skill title="Writer" details="I write anything from fiction to technical documents (though not both at the same time, that wouldn't do)." />
-                <Skill title="Seamstress" details="I have been sewing for over a decade. I sew men's & women's clothing, but most of the time, I sew <strong>my</strong> clothing." />
+                <Skill title="Seamstress" details="I have been sewing for over a decade. I sew men's & women's clothing, but most of the time, I sew my clothing." />
             </div>
         </section>
     </div>
