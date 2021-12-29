cfe44b958d762d80b58ff078ac0f3fcc2a699b12
Brenna <>
2021-12-29 12:11:07 -0700

    Create Contact component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./Navigation";
 import Intro from "./Intro";
 import Skills from "./Skills";
 import Testimonials from "./Testimonials";
+import Contact from "./Contact";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
@@ -11,16 +12,7 @@ export default function App() {
       <Intro />
       <Skills />
       <Testimonials />
-  {/* <!-- Contact Me --> */}
-
-  <section class="container-narrow white-section" id="contact-me">
-    <h2>Contact Me</h2>
-    <a href="https://github.com/bwatry"><i class="contact-icon fab fa-github-square fa-3x"></i></a>
-    <a href="https://www.linkedin.com/in/brenna-watry-9527b592/"><i class="contact-icon fab fa-linkedin fa-3x"></i></a>
-    <a href="https://www.facebook.com/brenna.watry"><i class="contact-icon fab fa-facebook-square fa-3x"></i></a>
-    {/* <!-- <a href="#contact-me"><i class="unlinked contact-icon fas fa-envelope-square fa-3x"></i></a> --> */}
-  </section>
-
+      <Contact />
   {/* <!-- Footer --> */}
 
   <footer id="footer" class="container-narrow white-section">
