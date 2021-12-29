65735f767d21ce1cf40f2996bf95a0a1facf230b
Brenna <>
2021-12-29 11:44:34 -0700

    Create Navigation component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,10 @@
 import React from "react";
+import Navigation from "./Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
     return <div>
-      {/* <!-- Navigation --> */}
-  <section class="teal-section">
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand" href="index.html">
-        <img class="nav-home" src="Images/Art Deco Logo-sm.png" alt="Home" />
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-        <li class="other-pages nav-item">
-        <a class="nav-link" href="about-me.html">About Me</a>
-        </li>
-        <li class="other-pages nav-item">
-        <a class="nav-link unlinked" href="javascript:void(0)">Unexpected Gift</a>
-        </li>
-      </ul>
-    </div>
-    </nav>
-  </section>
+    <Navigation />
 
   {/* <!-- Intro Section --> */}
   <section class="intro-section teal-section">
