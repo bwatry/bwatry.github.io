dabc540230a00cd5e77775ea4e3fabf82e6081f9
Brenna <>
2021-12-29 12:04:56 -0700

    Create Skills component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,14 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Intro from "./Intro";
+import Skills from "./Skills";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
     return <div>
-    <Navigation />
-    <Intro />
-
-  {/* <!-- About Me --> */}
-
-  <section class="container-fluid white-section">
-    <div class="row">
-      <div class="skill col-lg-4">
-        <h2>Programmer</h2>
-        <img class="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
-        <p class="skill-details">I have experience in HTML, CSS, JavaScript, SQL, NoSQL, & Python, as well as associated libraries.</p>
-      </div>
-      <div class="skill col-lg-4">
-        <h2>Writer</h2>
-        <img class="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
-        <p class="skill-details">I write anything from fiction to technical documents (though not both at the same time, that wouldn't do).</p>
-      </div>
-      <div class="skill col-lg-4">
-        <h2>Seamstress</h2>
-        <img class="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
-        <p class="skill-details">I have been sewing for over a decade. I sew men's & women's clothing, but most of the time, I sew <strong>my</strong> clothing.</p>
-      </div>
-    </div>
-
-  </section>
+      <Navigation />
+      <Intro />
+      <Skills />
 
   {/* <!-- Testimonials --> */}
 
---
+++ src/components/Skills.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Skills() {
+    return <div>
+        <section class="container-fluid white-section">
+            <div class="row">
+                <div class="skill col-lg-4">
+                    <h2>Programmer</h2>
+                    <img class="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
+                    <p class="skill-details">I have experience in HTML, CSS, JavaScript, SQL, NoSQL, & Python, as well as associated libraries.</p>
+                </div>
+                <div class="skill col-lg-4">
+                    <h2>Writer</h2>
+                    <img class="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
+                    <p class="skill-details">I write anything from fiction to technical documents (though not both at the same time, that wouldn't do).</p>
+                </div>
+                <div class="skill col-lg-4">
+                    <h2>Seamstress</h2>
+                    <img class="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
+                    <p class="skill-details">I have been sewing for over a decade. I sew men's & women's clothing, but most of the time, I sew <strong>my</strong> clothing.</p>
+                </div>
+            </div>
+        </section>
+    </div>
+}
