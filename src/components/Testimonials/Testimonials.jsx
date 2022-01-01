4fe29611905fd6d95a18a8dffbc5d321e7dd7bf1
Brenna <>
2022-01-01 15:31:49 -0700

    Remove excess divs

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,30 +1,23 @@
 import React from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
 
 export default function Testimonials() {
     return <Container fluid id="testimonials" className="teal-section">
-            <Row>
-                <Col lg="12">
-                    <Carousel id="testimonial-carousel" className="carousel slide" interval={8000}>
-                        <Carousel.Item className="container-fluid">
-                            <h3 className="testimonial-text">I told her for years that she should become a programmer. She finally listened to me.</h3>
-                            <em>Dad (father)</em>
-                        </Carousel.Item>
-                        <Carousel.Item className="container-fluid">
-                            <h3 className="testimonial-text">Brenna is a lifesaver! She just knows what to do and does it.</h3>
-                            <em>Every employer ever (not a direct quote)</em>
-                        </Carousel.Item>
-                        <Carousel.Item className="container-fluid">
-                            <h3 className="testimonial-text">She should be feeding us more. Like on the hour every hour. And she doesn't pet us enough. But otherwise, she's all right.</h3>
-                            <em>Saga & Rumor (cats)</em>
-                        </Carousel.Item>
-                    </Carousel>
-                    <img className="line-break-gold" src="Images/Art Deco Logo-sm.png" alt="Art Deco line break" />
-                </Col>
-            </Row>
+                <Carousel id="testimonial-carousel" className="carousel slide" interval={8000}>
+                    <Carousel.Item className="container-fluid">
+                        <h3 className="testimonial-text">I told her for years that she should become a programmer. She finally listened to me.</h3>
+                        <em>Dad (father)</em>
+                    </Carousel.Item>
+                    <Carousel.Item className="container-fluid">
+                        <h3 className="testimonial-text">Brenna is a lifesaver! She just knows what to do and does it.</h3>
+                        <em>Every employer ever (not a direct quote)</em>
+                    </Carousel.Item>
+                    <Carousel.Item className="container-fluid">
+                        <h3 className="testimonial-text">She should be feeding us more. Like on the hour every hour. And she doesn't pet us enough. But otherwise, she's all right.</h3>
+                        <em>Saga & Rumor (cats)</em>
+                    </Carousel.Item>
+                </Carousel>
+                <img className="line-break-gold" src="Images/Art Deco Logo-sm.png" alt="Art Deco line break" />
     </Container>
-
 }
