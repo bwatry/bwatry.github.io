7cd4c80abff7a4a7fcf517847e0eb920750c5aa2
Brenna <>
2022-01-01 10:51:12 -0700

    Update Navigation to React-Bootstrap styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,17 @@
 import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
 
 export default function Navigation() {
-    return <div>
-        <section className="teal-section">
-            <nav className="navbar navbar-expand-lg navbar-dark">
-                <a className="navbar-brand" href="index.html">
+    return <Navbar className="teal-section" expand="lg" variant="dark" sticky="top">
+                <Navbar.Brand href="index.html">
                     <img className="nav-home" src="Images/Art Deco Logo-sm.png" alt="Home" />
-                </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="other-pages nav-item">
-                            <a className="nav-link" href="about-me.html">About Me</a>
-                        </li>
-                        <li className="other-pages nav-item">
-                            <a className="nav-link unlinked" href="story.html">Unexpected Gift</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </section>
-    </div>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link className="other-pages" href="about-me.html">About Me</Nav.Link>
+                        <Nav.Link className="other-pages" href="story.html">Unexpected Gift</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
 }
