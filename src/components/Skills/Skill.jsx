40b43470f76f4bde111f36b1622af0e1b523264e
Brenna <>
2022-01-01 15:05:03 -0700

    Add responsive styling to skills

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 
 export default function Skill(props) {
-    return <Col lg="4" className="skill">
+    return <Col sm="auto" lg="4" className="skill">
         <h2>{props.title}</h2>
         <img className="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
         <p className="skill-details">{props.details}</p>
