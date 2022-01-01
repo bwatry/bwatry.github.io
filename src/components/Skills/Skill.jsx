5510f7d749d332b1dc2fe32e77d43d9769ec0ba1
Brenna <>
2022-01-01 14:57:16 -0700

    Update Skills to react-bootstrap

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
 
 export default function Skill(props) {
-    return <div className="skill col-lg-4">
+    return <Col lg="4" className="skill">
         <h2>{props.title}</h2>
         <img className="line-break-teal" src="Images/Art Deco Logo Teal.png" alt="Art Deco line break" />
         <p className="skill-details">{props.details}</p>
-    </div>
+    </Col>
 }
