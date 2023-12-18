4893fcbbf566416f8b8d2a9b4de238ecef324bad
Brenna <brenna@breezyfohn.com>
2023-12-17 17:10:35 -0700

    improve accessibility score

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,8 +5,14 @@ import "./contact.css";
 export default function Contact() {
     return <Container className="container-narrow white-section" id="contact-me">
             <h2>Contact Me</h2>
-            <a href="https://github.com/bwatry"><i className="contact-icon fab fa-github-square fa-3x"></i></a>
-            <a href="https://www.linkedin.com/in/brenna-watry-9527b592/"><i className="contact-icon fab fa-linkedin fa-3x"></i></a>
-            <a href="https://www.facebook.com/brenna.watry"><i className="contact-icon fab fa-facebook-square fa-3x"></i></a>
+            <a href="https://github.com/bwatry" aria-label="Go to Brenna's GitHub page." tabIndex="0">
+                <i className="contact-icon fab fa-github-square fa-3x"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/brenna-watry-9527b592/" aria-label="Go to Brenna's LinkedIn page." tabIndex="0">
+                <i className="contact-icon fab fa-linkedin fa-3x"></i>
+            </a>
+            <a href="https://www.facebook.com/brenna.watry" aria-label="Go to Brenna's Facebook page." tabIndex="0">
+                <i className="contact-icon fab fa-facebook-square fa-3x"></i>
+            </a>
         </Container>
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,15 +6,15 @@ import "./navigation.css";
 
 export default function Navigation() {
     return <Navbar className="teal-section" expand="lg" variant="dark" sticky="top">
-                <Navbar.Brand href="/">
-                    <img className="nav-home" src="Images/Art Deco Logo-sm.png" alt="Home" />
+                <Navbar.Brand href="/" tabIndex="0">
+                    <img className="nav-home" src="Images/Art Deco Logo-sm.png" alt="Go to Home Page" title="Go to Home Page" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link className="other-pages" as={Link} to="/about">About Me</Nav.Link>
-                        <Nav.Link className="other-pages" as={Link} to="//breezyfohn.com/#/cancer">My Journey</Nav.Link>
-                        <Nav.Link className="other-pages" as={Link} to="/story">Unexpected Gift</Nav.Link>
+                        <Nav.Link className="other-pages" as={Link} to="/about" tabIndex="0">About Me</Nav.Link>
+                        <Nav.Link className="other-pages" as={Link} to="//breezyfohn.com/#/cancer" tabIndex="0">My Journey</Nav.Link>
+                        <Nav.Link className="other-pages" as={Link} to="/story" tabIndex="0">Unexpected Gift</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
